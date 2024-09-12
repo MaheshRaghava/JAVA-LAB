0ba032fa5da9b5cf558de8d442fc42f96d17b671
Mahesh Raghava <160461240+MaheshRaghava@users.noreply.github.com>
2024-09-12 11:40:48 +0530

    Update Binary Search.java

--- Searching Techniques/Binary Search.java
+++ Searching Techniques/Binary Search.java
@@ -1,66 +1,72 @@
 import java.util.*;
-class binarysearch
+class Binary_search
 {
-  public static void main(String args[])
-  {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter size of array : ");
-    int n = sc.nextInt();
-    int arr[] = new int[n];
-    int i;
-    System.out.println("Enter the elements : ");
-    for (i = 0; i < n; i++)
-    {
-      arr[i] = sc.nextInt();
-    }
-    int start = 0, end = n - 1, mid;
-    int pos = -1;
-    System.out.println("Enter element to search : ");
-    int ele = sc.nextInt();
-    while (start <= end) 
-    {
-      mid = (start + end) / 2;
-      if (arr[mid] == ele)
-      {
-        pos = mid + 1;
-        break;
-      }
-      else if (ele < arr[mid])
-      {
-        end = mid - 1;
-      } 
-      else 
-      {
-        start = mid + 1;
-      }
-    }
-    if (pos > 0)
-    {
-      System.out.printf("Element found at %d",pos);
-    }
-    else
-    {
-      System.out.println("Element is not found");
-    }
-  }
+static void sort(int a[],int n)
+{
+	int t;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n-i-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				t = a[j];
+				a[j]=a[j+1];
+				a[j+1]=t;
+			}
+		}
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+static int Binarysearch(int a[],int n,int key)
+{
+	
+        int start=0,end=n-1,pos=-1;
+        while(start<=end)
+        {
+        	int mid=(start+end)/2;
+        	if(a[mid]==key)
+        	{
+        		pos = mid+1;
+        		break;
+        	}
+        	else if(key<a[mid])
+        	{
+        		end = mid-1;
+        	}
+        	else
+        	{
+        		start = mid+1;
+        	}
+        }
+        if(pos>0)
+        {
+        	System.out.println("key is found at "+pos);
+        }
+        else
+        {
+        	System.out.println("key is not found");
+        }
+        return pos;
+}
+public static void main(String []args)
+{
+	
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter the array size:");
+	int n = sc.nextInt();
+	int a[] = new int[n];
+	System.out.println("enter array elements:");
+	for(int i=0;i<n;i++){
+	a[i]=sc.nextInt();
+	}
+	sort(a,n);
+        System.out.println("the sorted elements are:");
+	for(int i=0;i<n;i++){
+	System.out.println(a[i]);
+	}
+        System.out.println("enter a key:");
+        int key = sc.nextInt();
+        Binarysearch(a,n,key);
+        
+}       
+}
